95cc67212e8a515ea1bf487edf2090608b4a108d
Clive Jesus <clivejesusjohn@gmail.com>
2026-02-24 20:41:19 +0300

    feat: update meta description and adjust spacing in Index component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,6 +12,7 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
+      
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
